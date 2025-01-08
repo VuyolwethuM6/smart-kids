e96657947a0be3478e749a7cc2131e6e0fb7ae23
Vuyolwethu Mabhuleka <142888677+VuyolwethuM6@users.noreply.github.com>
2025-01-08 22:50:01 +0200

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,7 +11,7 @@ export default function BlogPage() {
     <div className="container mx-auto px-4 py-8">
       
       <h1 className="text-4xl font-bold text-center mb-12 text-gray-800">
-        MSI Blog: Insights & Stories
+        Smart Kids: Insights & Stories
       </h1>
 
       {posts.length === 0 ? (
